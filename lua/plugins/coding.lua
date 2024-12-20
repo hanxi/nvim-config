f0ca66bdcfb32c0769cf740515d8ca34794d69e8
涵曦 <im.hanxi@gmail.com>
2024-12-20 22:05:03 +0800

    update

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -6,4 +6,21 @@ return {
     {
         "kazhala/close-buffers.nvim",
     },
+
+	{
+		"saghen/blink.cmp",
+		opts = {
+			keymap = {
+				preset = "enter",
+				["<Tab>"] = {
+					"select_next",
+					"fallback",
+				},
+				["<S-Tab>"] = {
+					"select_prev",
+					"fallback",
+				},
+			},
+		},
+	},
 }
