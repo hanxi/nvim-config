494db4eb6d5fca8bf52c88d07780483621767275
涵曦 <im.hanxi@gmail.com>
2024-12-23 17:03:14 +0800

    update

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -72,10 +72,10 @@ return {
 				['--cycle'] = true,
 			},
 			files = {
-				rg_opts = '--color=never --files --hidden --follow --glob "*.proto" --glob "error_code.go"',
+				rg_opts = '--color=never --files --hidden --follow --no-ignore --glob "!*.pb.go"',
 			},
 			grep = {
-				rg_opts = '--color=never --column --line-number --no-heading --smart-case --hidden --follow --glob "*.proto" --glob "error_code.go"',
+				rg_opts = '--color=never --column --line-number --no-heading --smart-case --hidden --follow --no-ignore --glob "!*.pb.go"',
 			}
 		},
 	}
